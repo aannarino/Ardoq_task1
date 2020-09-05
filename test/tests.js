0882b5383905fa2d194490cc3c9fbece5f1f439d
K1450823 <k1450823@kingston.ac.uk>
2020-09-05 15:01:10 +0100

    Implemented testing libraries

---
+++ test/tests.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+
+//const greatestProduct = require()
+describe('Testing test suite', function(){
+    it('Should return a pass', function(){
+        expect(1).to.equal(1);
+    })
+})
